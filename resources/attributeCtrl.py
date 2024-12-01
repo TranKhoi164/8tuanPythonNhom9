fa7294f762dd122e5a552d698ba530dbae767b4e
truonghieu3111 <hieutruong3107@gmail.com>
2024-12-01 23:44:40 +0700

    attributeCtrl.py

---
+++ resources/attributeCtrl.py
@@ -0,0 +1,24 @@
+colors = [
+    ["Trắng"],
+    ["Đen"],
+    ["Đỏ"],
+    ["Hồng"]
+]
+
+sizes = [
+    "M",
+    "L",
+    "XL",
+    "XXL",
+    "3XL"
+]
+
+# Hiển thị danh sách màu
+print("Màu:")
+for row in colors:
+    print("  ".join(row))
+
+# Hiển thị danh sách size
+print("\nSize:")
+for size in sizes:
+    print(size)
