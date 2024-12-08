09ca87639fbbb457af40bd4b4abf77d64244973c
kehuydie with Quang <minhquangngutoan@gmail.com>
2024-12-08 10:15:33 +0700

    up_date

--- resources/inventoryCtrl.py
+++ resources/inventoryCtrl.py
@@ -1,97 +1,53 @@
-from .utils.globalVar import Inventories
+from .utils.globalVar import Inventories ,collectionIds
 
 
 
 def them_san_pham(n):
-    iD=Inventories[-1]['id']
-    dictatribute={'mau':'chua_co','kich thuoc':'chua_co'}
-    
-    a=['id','product','attribute','quantity','price']
-    
-    bienmacdinh='chua_co'
-    
-    b=dict.fromkeys(a,bienmacdinh)
-    
-    b['id']=iD+1
-    
-    b['product']=int(input('moi nhap product: '))
-    
-    print('moi nhap attribute:')
-    
-    b['attribute']=dictatribute
-    
-    b["attribute"]['mau']=input('moi nhap mau: ')
-    
-    b["attribute"]['kich thuoc']=input('moi nhap size: ')
-    
-    b['quantity']=int(input('moi nhap so luong: '))
-    
-    b['price']=int(input('moi nhap gia sp: '))
-    
-    Inventories.append(b)
-    print(Inventories)
+    try:
+        newinventory={
+            "product": None,
+            "attribute": {"màu ": None, "kích thước": None},
+            "quantity": None,
+            "price": None,
+        }
 
-
-def xoa_san_pham(n):
-    n=int(input('nhap id san pham muon xoa: '))
-    for i in Inventories:
-        if i['id']==n:
-            Inventories.remove(i)
-    print(Inventories)
-
-def suasoluong(n):
-    print('ban co muon sua so_luong ?')
-    print('1. YES')
-    print('2. NO')
-    traloi=int(input())
-    if traloi==1:
-        n['quantity']=int(input('moi nhap so luong moi: '))
-        print(Inventories)
-    elif traloi==2:
-        suagiaca(n)
-    else:
-        unavailableOption()
-        suasoluong(n)
-
-
-def suagiaca(n):
-    print('ban co muon sua gia cua san pham ?')
-    print('1. YES')
-    print('2. NO')
-    traloi=int(input())
-    if traloi==1:
-        n['price']=int(input('moi nhap gia moi: '))
-        print(Inventories)
-    elif traloi==2:
-        suathuoctinh(n)
-    else:
-        unavailableOption()
-        suasoluong(n)
-
-
-def suathuoctinh(n):
-    print('ban co muon sua thuoctinh ?')
-    print('1. YES')
-    print('2. NO')
-    traloi=int(input())
-    if traloi==1:
-        n["attribute"]["màu "]=input('moi nhap mau moi: ')
-        n["attribute"]["kích thước"]=input('moi nhap size moi: ')
-        print(Inventories)
-
+        newinventory=n       
+        collectionIds['inventoryId']+=1
+        newinventoryID = {'id':collectionIds['inventory']}
+        newinventoryID.update(newinventory)
+        Inventories.append(newinventoryID)
         
-def update_san_pham():
-    n=int(input('nhap id san pham muon sua: '))
-    for i in Inventories:
-        if i['id']==n:
-            suasoluong(i)
-            
+        msd='thành công!'
+        return msd
+    except:
+        mss="LỖI"
+        return mss
 
 
-def unavailableOption():
-    print('----Lựa chọn không khả dụng----\n')
+def showinventory(n):
+    n=int(n)
+    listinvent=[]
+    for inve in Inventories:
+        if inve["product"]==n:
+            listinvent.append(inve)
+    return listinvent
 
 
+def showIN4inventory(n):
+    n=int(n)
+    listtrave=[]
+    for inve in Inventories:
+        if inve["id"]==n:
+            listtrave.append(inve)
+    return listtrave
 
-#while True:
-    #update_san_pham()
+
+def update_san_pham(n):
+    for i in range(len(Inventories)):
+        if Inventories[i]["id"]==n['id']:
+            Inventories[i]['quantity']=n['quantity']
+            Inventories[i]['price']=n['price']
+            return Inventories
+    
+
+
