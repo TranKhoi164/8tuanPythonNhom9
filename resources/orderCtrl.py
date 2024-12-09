22b06f1c4a2c5e5ecee784b7cbe21221edae3ba6
Kkidcon <buiphamkhanh97@gmail.com>
2024-12-09 22:14:38 +0700

    update orderCtrl

--- resources/orderCtrl.py
+++ resources/orderCtrl.py
@@ -1,9 +1,22 @@
-# from utils.globalVar import Orders, collectionIds
-from resources.utils.globalVar import Orders, collectionIds
+from utils.globalVar import Orders, collectionIds
+from utils.handleExceptions import elementNotFound, unavailableOption
+# from resources.utils.globalVar import Orders, collectionIds
 from datetime import datetime
 
 # todo: khoi them
 now = datetime.now()
+
+
+def getOrders():
+  global Orders
+  return Orders
+
+def getOrderById(orderId):
+  order = list(filter(lambda order: order['id'] == orderId, Orders))
+  return order[0]
+
+# print(getOrderById(0))
+
 def createOrder(orderDict):
   try:
     newOrder = orderDict.copy()
@@ -16,11 +29,23 @@ def createOrder(orderDict):
   except:
     raise Exception('Thêm order thất bại!')
 
-# todo test createOrder function
+def deleteOrders(orderId):
+  try :
+    orderDelete = list(filter(lambda orderDel: orderDel['id'] == orderId, Orders))
+    Orders.remove(orderDelete[0])
+    return 'Xóa thành công!'
+  except Exception :
+    raise Exception(elementNotFound('đơn hàng'))
+
+
+# # todo test createOrder function
 # newOrder = {"product": 0,
 #             "inventory": 0,
 #             "address": 0,
 #             "status": "done",
-#             "quantity": 2, }
+#             "quantity": 2 }
 # createOrder(newOrder)
+# createOrder(newOrder)
+# deleteOrders(3)
 # print(Orders)
+
