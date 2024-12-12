7296e68dc11455f1617cfb39052f268584444863
Kkidcon <buiphamkhanh97@gmail.com>
2024-12-12 16:44:34 +0700

    update OrderClient

--- resources/orderClient.py
+++ resources/orderClient.py
@@ -3,27 +3,25 @@
 # from resources import inventoryCtrl
 # from resources.orderCtrl import createOrder, getOrderById
 
+from utils.globalVar import state
 from utils.stringFunc import updatePriceStr
 from productCtrl import getProductById
 from inventoryCtrl import getInventoryById
-from orderCtrl import createOrder, getOrderById
+from orderCtrl import createOrder, getOrderById, getOrderByStatus
 
 
 def showOrderInfo(idOrder):
     order = getOrderById(idOrder)
-    product = getProductById(order['product'])
-    orderInventory = getInventoryById(order['inventory'])
-    namePro = product['name']
-    print(f'Đơn hàng tạo vào lúc: {order['createdAt']}')
-    print('Số lượng:', order['quantity'])
-    print('Sản phẩm:', namePro)
-    print(f'Giá tiền: {updatePriceStr(product['price'] * order['quantity'])}')
-    print('Loại sản phẩm: ', orderInventory['attribute'])
-    
     if order['status'] == 'done' :
-        print('Trạng thái: Đã thanh toán!')
-    else :
-        print('Trạng thái: Chưa thanh toán!')
+        product = getProductById(order['product'])
+        orderInventory = getInventoryById(order['inventory'])
+        namePro = product['name']
+        print(f'Đơn hàng tạo vào lúc: {order['createdAt']}')
+        print('Số lượng:', order['quantity'])
+        print('Sản phẩm:', namePro)
+        print(f'Giá tiền: {updatePriceStr(orderInventory['price'] * order['quantity'])}')
+        print('Loại sản phẩm: ', orderInventory['attribute'])
+        print('Thanh toán thành công!')
 
 def showOrdersInfor(orderIds):
     for i in orderIds:
@@ -32,4 +30,4 @@ def showOrdersInfor(orderIds):
 
 
 # todo test createOrder function
-# showOrdersInfor([0])
+showOrdersInfor([0])
