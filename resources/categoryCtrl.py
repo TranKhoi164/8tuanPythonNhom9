7db493ca598c952927b4e16725f08f6e636446e1
truonghieu3111 <hieutruong3107@gmail.com>
2024-12-13 09:05:36 +0700

    cateService

--- resources/categoryCtrl.py
+++ resources/categoryCtrl.py
@@ -1,12 +1,65 @@
-import resources.utils.globalVar as globalVar
-import resources.utils.handleExceptions as error
+from utils.globalVar import Categories, collectionIds
+from utils.handleExceptions import elementNotFound
+# from resources.utils.globalVar import state, Categories
 
-def getCategories():
-  return globalVar.Categories
+#state 200
+def createCategory(categoryDict):
+  try:
+    categorynew = categoryDict.copy()
+    collectionIds['categoryId'] += 1
+    categorynew['id'] = collectionIds['categoryId']
+    Categories.append(categorynew)
+    return categorynew
+  except:
+    raise Exception('')
 
-def getCategoryById(cateId):
-  reqCategory = list(filter(
-        lambda i: i['id'] == cateId, globalVar.Categories))
-  if len(reqCategory) == 0:
-        raise Exception(error.elementNotFound('danh mục'))
-  return reqCategory[len(reqCategory)-1]
+# todo test category
+# newCategory = {'name': 'test'}
+# createCategory(newCategory)
+# print(Categories)
+
+def deleteCategoryById(CategoryId):
+  global Categories
+  try:
+    reqCategory = list(filter(lambda category: category['id'] == CategoryId, Categories))
+    Categories.remove(reqCategory[0])
+
+    msg = 'Xóa danh mục thành công!'
+    return msg
+  except Exception as e: 
+    raise Exception(elementNotFound('danh mục'))
+  
+  #todo test category
+# deleteId = 0
+# deleteCategoryById(deleteId)
+# print(Categories)
+
+
+def createCategory(categoryDict):
+  global Categories, collectionIds
+  collectionIds['categoryId'] = collectionIds['categoryId'] + 1
+  categoryDict['id'] = collectionIds['categoryId']
+  Categories.append(categoryDict)
+
+  msg = 'Tạo danh mục thành công!'
+  return {'msg': msg, 'category': categoryDict}
+
+
+def updateCategoryById(categoryDict):
+  try:
+    global Categories
+
+    reqCategory = list(filter(lambda category: category['id'] == categoryDict['id'], Categories))
+    Indx = Categories.index(reqCategory[0])
+
+    if len(reqCategory) == 0:
+      raise Exception(elementNotFound("danh mục"))
+
+    for key in categoryDict: 
+      Categories[Indx][key] = categoryDict[key]
+    
+    msg = 'cập nhật danh mục thành công!'
+    return {'msg': msg}
+  except:
+    raise Exception(elementNotFound("danh mục"))
+
